896f269bdcf5a89807a1d07936226547caae5b33
kacper.lagowski <kacper.lagowski@senecaim.com>
2016-05-10 11:42:47 +0100

    In the middle of importing data into the OLV

---
+++ Portfolio/Portfolio/Instrument.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio
+{
+    public class Instrument
+    {
+        public Stock()
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            SqlCommand cmd = Database.CommandFactory("spInstrumentDate");
+            da.SelectCommand = cmd;
+            da.Fill(ds);
+            DataTable dtInstrument = ds.Tables[0];            
+            
+            return 
+        }
+}
